d8a4c7a3f53e7e45e0fd4c29454b39aac7d66eac
Adam Lamont <adam_lamont@wrdsb.ca>
2020-10-04 11:57:39 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program names the animal by asking user its personalities
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +11,40 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
+    //create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask user if animal has feathers
+     System.out.println("Does the animal have feathers? (yes/no)");
+     String featherAnswer = input.nextLine();
+    //check to see feather Statements
+     if(featherAnswer.equals("yes")){
+    //see if the animal can swim
+      System.out.println("Can the animal swim? (yes/no)")
+      ;
+      String swimAnswer = input.nextLine();
+    //determin animal
+      if(swimAnswer.equals("yes")){
+       System.out.println("This is a duck");
+      } else if(swimAnswer.equals("no")){
+        System.out.println("This is a hen");
+      }
+     }
+    //check to see feather Statements
+     if(featherAnswer.equals("no")){
+    //see if the animal can swim
+      System.out.println("does the animal have legs? (yes/no)")
+      ;
+      String legsAnswer = input.nextLine();
+    //determin animal
+      if(legsAnswer.equals("yes")){
+       System.out.println("This is a lizard");
+      } else if(legsAnswer.equals("no")){
+        System.out.println("This is a snake");
+     }
+     } 
+     }
 }
+     
+    
+
